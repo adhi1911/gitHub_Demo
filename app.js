b1b62d48a48b598909e9dd892913db83b5526e3a
adhi1911 <amk2754@gmail.com>
2023-11-20 14:52:01 +0530

    Add New Feature

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-console.log("hello world")
-let name = "Arya"
+console.log("hello world");
+let name = "Arya";
 const num = 40;
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+const result = calculateSum(10, 20);
+console.log("The sum is:", result);
